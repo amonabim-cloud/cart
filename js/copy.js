1b821ddea2a967f4618d4854b83e48ddea01cff4
Amounabi <amonabi.m@gmail.com>
2026-07-17 08:41:55 +0330

    Update copy.js

--- js/copy.js
+++ js/copy.js
@@ -1,1 +1,11 @@
+function copyText(text) {
 
+    navigator.clipboard.writeText(text)
+    .then(() => {
+        alert("کپی شد ✅");
+    })
+    .catch(() => {
+        alert("خطا در کپی");
+    });
+
+}
