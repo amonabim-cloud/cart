28e63b45cd82bdc440a0e0ba89cc55f33bf112c4
Amounabi <amonabi.m@gmail.com>
2026-07-17 08:38:11 +0330

    Refactor fetch calls for better readability

--- js/app.js
+++ js/app.js
@@ -1,35 +1,59 @@
 async function loadData() {
 
-  const people = await fetch("data/people.json")
-    .then(response => response.json());
+const people = await fetch("data/people.json")
+.then(r => r.json());
 
-  const banks = await fetch("data/banks.json")
-    .then(response => response.json());
+const banks = await fetch("data/banks.json")
+.then(r => r.json());
 
-  const cards = await fetch("data/cards.json")
-    .then(response => response.json());
-
-
-  cards.forEach(card => {
-
-    const person = people.find(
-      p => p.id === card.personId
-    );
-
-    const bank = banks.find(
-      b => b.id === card.bankId
-    );
+const cards = await fetch("data/cards.json")
+.then(r => r.json());
 
 
-    console.log({
-      صاحب: person.name,
-      بانک: bank.name,
-      شماره: card.cardNumber
-    });
+const container = document.getElementById("cards");
 
-  });
+
+cards.forEach(card => {
+
+const person = people.find(
+p => p.id === card.personId
+);
+
+const bank = banks.find(
+b => b.id === card.bankId
+);
+
+
+container.innerHTML += `
+
+<div class="card">
+
+<h3>${bank.name}</h3>
+
+<div class="number">
+${card.cardNumber}
+</div>
+
+<p>
+👤 ${person.name}
+</p>
+
+<p>
+شبا:
+${card.sheba}
+</p>
+
+<p>
+حساب:
+${card.accountNumber}
+</p>
+
+</div>
+
+`;
+
+});
 
 }
-
 
 loadData();
